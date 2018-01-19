c05bc275ee5b3708ccb223d606b99ec9cb0b216f
Edsger Lin <edsgerlin@outlook.com>
2018-01-20 00:19:57 +0800

    Implemented a simple LRU Cache.

---
+++ src/lruCache.ts
@@ -0,0 +1,47 @@
+class LruCache<K, V> {
+  public readonly capacity: number;
+  private map: Map<K, [V, IDoublyLinkedNode<K>]> = new Map<K, [V, IDoublyLinkedNode<K>]>();
+  private list: DoublyLinkedList<K> = new DoublyLinkedList<K>();
+  public constructor(capacity: number) {
+    if (capacity <= 0) {
+      throw Error("LruCache: capacity must be greater then zero.");
+    }
+    this.capacity = capacity;
+  }
+  public get size(): number {
+    return this.map.size;
+  }
+  public isFull(): boolean {
+    return this.capacity === this.size;
+  }
+  public set(key: K, value: V) {
+    const pair = this.map.get(key);
+    if (pair) {
+      const [, oldNode] = pair;
+      this.list.removeNode(oldNode);
+      this.map.delete(key);
+    }
+    if (this.isFull()) {
+      const lastNode = this.list.last;
+      if (lastNode === undefined) {
+        throw Error("LruCache: cache is full but no node to remove.");
+      }
+      this.map.delete(lastNode.value);
+      this.list.removeLast();
+    }
+    const node = this.list.append(key);
+    this.map.set(key, [value, node]);
+  }
+
+  public get(key: K): V | undefined {
+    const pair = this.map.get(key);
+    if (!pair) {
+      return undefined;
+    }
+    const [value, oldNode] = pair;
+    this.list.removeNode(oldNode);
+    const newNode = this.list.append(key);
+    this.map.set(key, [value, newNode]);
+    return value;
+  }
+}
